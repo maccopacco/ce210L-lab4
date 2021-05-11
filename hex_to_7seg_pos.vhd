d5f7a72cc7ede2c688c805dedd4a5c6f754deeda
Max Dreher <maxdmax101@gmail.com>
2021-05-11 15:37:37 -0400

    Procedure 2 - Question 2 Update hex

---
+++ hex_to_7seg_pos.vhd
@@ -0,0 +1,34 @@
+-- Writes to hex light with POS
+
+LIBRARY ieee;
+USE ieee.std_logic_1164.all;
+
+ENTITY hex_to_7seg_pos IS
+   PORT( S   :IN  STD_LOGIC_VECTOR(3 DOWNTO 0);
+         HEX :OUT STD_LOGIC_VECTOR(6 DOWNTO 0)
+	);
+END ENTITY;
+
+ARCHITECTURE CANONICAL_POS of hex_to_7seg_pos IS
+
+   ALIAS S3 : STD_LOGIC is S(3);
+   ALIAS S2 : STD_LOGIC is S(2);
+   ALIAS S1 : STD_LOGIC is S(1);
+   ALIAS S0 : STD_LOGIC is S(0);
+   ALIAS a : STD_LOGIC is HEX(0);
+   ALIAS b : STD_LOGIC is HEX(1);
+   ALIAS c : STD_LOGIC is HEX(2);
+   ALIAS d : STD_LOGIC is HEX(3);
+   ALIAS e : STD_LOGIC is HEX(4);
+   ALIAS f : STD_LOGIC is HEX(5);
+   ALIAS g : STD_LOGIC is HEX(6);   
+
+BEGIN
+   a <= not( (S3 OR S2 OR S1 OR NOT(S0)) AND (S3 OR NOT(S2) OR S1 OR S0) AND (NOT(S3) OR S2 OR NOT(S1) OR NOT(S0)) AND (NOT(S3) OR NOT(S2) OR S1 OR NOT(S0))  ); -- put your equations inside the not() operations
+   b <= not( (NOT(S2) OR NOT(S1) OR S0) AND (NOT(S3) OR NOT(S1) OR NOT(S0)) AND (NOT(S3) OR NOT(S2) OR S0) AND (S3 OR NOT(S2) OR S1 OR NOT(S0)));
+   c <= not( (NOT(S3) OR NOT(S2) OR S0) AND (NOT(S3) OR NOT(S2) OR NOT(S1)) AND (S3 OR S2 OR NOT(S1) OR S0));
+   d <= not( (NOT(S2) OR NOT(S1) OR NOT(S0)) AND (S3 OR S2 OR S1 OR NOT(S0)) AND (S3 OR NOT(S2) OR S1 OR S0) AND (NOT(S3) OR S2 OR NOT(S1) OR S0) );
+   e <= not( (S3 OR NOT(S0)) AND (S2 OR S1 OR NOT(S0)) AND (S3 OR NOT(S2) OR S1) );
+   f <= not( (S3 OR S2 OR NOT(S0)) AND (S3 OR S2 OR NOT(S1)) AND (S3 OR NOT(S1) OR NOT(S0)) AND (NOT(S3) OR NOT(S2) OR S1 OR NOT(S0))  );
+   g <= not( (S3 OR S2 OR S1) AND (S3 OR NOT(S2) OR NOT(S1) OR NOT(S0)) AND (NOT(S3) OR NOT(S2) OR S1 OR S0)  );
+END CANONICAL_POS;
--- lab4_pos.vhd
+++ lab4_pos.vhd
@@ -1,6 +1,6 @@
 -- Author: Max Dreher 
 -- Last Modified: 5/11/2021
--- Purpose: use the DE10-Lite 7-segment LED displays and demonstrate canonical sum of products 
+-- Purpose: use the DE10-Lite 7-segment LED displays and demonstrate canonical product of sums 
 
 LIBRARY ieee;
 USE ieee.std_logic_1164.all;
@@ -13,12 +13,12 @@ END lab4_pos;
 
 ARCHITECTURE can_pos OF lab4_pos IS
 
-   COMPONENT hex_to_7seg
+   COMPONENT hex_to_7seg_pos
 	   PORT( S	  :IN	   STD_LOGIC_VECTOR(3 DOWNTO 0);
             HEX  :OUT   STD_LOGIC_VECTOR(6 DOWNTO 0)
 		);
    END COMPONENT;
  
 BEGIN
-   U0 : hex_to_7seg port map (SW(9 DOWNTO 6), HEX5);
+   U0 : hex_to_7seg_pos port map (SW(9 DOWNTO 6), HEX5);
 END can_pos;
